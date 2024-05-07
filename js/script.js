c71327db4f9196820d9eee10d3d0fb858a7d1ae5
islo <islomabduqodir@gmail.com>
2024-05-07 22:36:09 +0500

    send messages

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+function sendRequest(nameSelector, phoneSelector) {
+    let name = document.querySelector(nameSelector).value;
+    let phone = document.querySelector(phoneSelector).value;
+    let requestText = encodeURI(`<b>Заявка</b>\nИмя: ${name}\nНомер: ${phone}`)
+    fetch(`https://api.telegram.org/bot6763716350:AAFvDCuttNTObJ2HsaFMGekhCGBPL-fDOiE/sendMessage?text=${requestText}&chat_id=-4255284045&parse_mode=HTML`)
+}
+
+document.querySelector('.feature-consultation-form button').addEventListener('click', (value) => {
+    value.preventDefault()
+    sendRequest(`.feature-consultation-form input[name="name"]`, `.feature-consultation-form input[name="phone"]`)
+})
+
+document.querySelector('#form-modal button').addEventListener('click', (value) => {
+    value.preventDefault()
+    sendRequest(`#form-modal input[name="name"]`, `#form-modal input[name="phone"]`)
+})
+
 MicroModal.init({
     openTrigger: 'data-custom-open',
     closeTrigger: 'data-custom-close',
@@ -21,16 +38,16 @@ let totalNumber;
 let currentNumber;
 
 const myswiper = new Swiper(".swiper", {
-  speed: 800, // スライドが切り替わる時の速さ
-  slidesPerView: "3", // スライド表示数
-  spaceBetween: 50, // スライド間の距離
-  loop: true, // ループさせる
-  centeredSlides: true, // スライドを中央に配置にする
-  autoplay: {
-    // 自動再生させる
-    delay: 1500, // 次のスライドまでの秒数
-    disableOnInteraction: true // ユーザーがスライダー操作後も自動再生し続ける
-  }
+    speed: 800, // スライドが切り替わる時の速さ
+    slidesPerView: "3", // スライド表示数
+    spaceBetween: 50, // スライド間の距離
+    loop: true, // ループさせる
+    centeredSlides: true, // スライドを中央に配置にする
+    autoplay: {
+        // 自動再生させる
+        delay: 1500, // 次のスライドまでの秒数
+        disableOnInteraction: true // ユーザーがスライダー操作後も自動再生し続ける
+    }
 });
 
 // 
@@ -39,9 +56,16 @@ $(document).ready(
     function () {
         $(".log-in").click(
             function () {
-                $(".panel_call").animate({width:'toggle'}, 350)
+                $(".panel_call").animate({
+                    width: 'toggle'
+                }, 350)
                 return false;
             }
         )
     }
 );
+
+// 
+
+
+
