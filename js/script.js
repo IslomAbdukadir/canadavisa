8903b2c862f18c2fb92a737ce1ff3d9d93e5c481
islomabduqodir <islomabduqodir@gmail.com>
2024-05-07 17:31:18 +0500

    @1

--- js/script.js
+++ js/script.js
@@ -15,91 +15,23 @@ $('[data-custom-close]').click(function () {
 
 // 
 
-jQuery(document).ready(function ($) {
-    var feedbackSlider = $(".feedback-slider");
-    feedbackSlider.owlCarousel({
-        items: 1,
-        nav: true,
-        dots: true,
-        autoplay: true,
-        loop: true,
-        mouseDrag: true,
-        touchDrag: true,
-        navText: [
-            "<i class='fa fa-long-arrow-left'></i>",
-            "<i class='fa fa-long-arrow-right'></i>"
-        ],
-        responsive: {
-            // breakpoint from 767 up
-            767: {
-                nav: true,
-                dots: false
-            }
-        }
-    });
+let total = document.querySelector(".total");
+let current = document.querySelector(".current");
+let totalNumber;
+let currentNumber;
 
-    feedbackSlider.on("translate.owl.carousel", function () {
-        $(".feedback-slider-item h3")
-            .removeClass("animated fadeIn")
-            .css("opacity", "0");
-        $(".feedback-slider-item img, .feedback-slider-thumb img, .customer-rating")
-            .removeClass("animated zoomIn")
-            .css("opacity", "0");
-    });
-
-    feedbackSlider.on("translated.owl.carousel", function () {
-        $(".feedback-slider-item h3").addClass("animated fadeIn").css("opacity", "1");
-        $(".feedback-slider-item img, .feedback-slider-thumb img, .customer-rating")
-            .addClass("animated zoomIn")
-            .css("opacity", "1");
-    });
-    feedbackSlider.on("changed.owl.carousel", function (property) {
-        var current = property.item.index;
-        var prevThumb = $(property.target)
-            .find(".owl-item")
-            .eq(current)
-            .prev()
-            .find("img")
-            .attr("src");
-        var nextThumb = $(property.target)
-            .find(".owl-item")
-            .eq(current)
-            .next()
-            .find("img")
-            .attr("src");
-        var prevRating = $(property.target)
-            .find(".owl-item")
-            .eq(current)
-            .prev()
-            .find("span")
-            .attr("data-rating");
-        var nextRating = $(property.target)
-            .find(".owl-item")
-            .eq(current)
-            .next()
-            .find("span")
-            .attr("data-rating");
-        $(".thumb-prev").find("img").attr("src", prevThumb);
-        $(".thumb-next").find("img").attr("src", nextThumb);
-        $(".thumb-prev")
-            .find("span")
-            .next()
-            .html(prevRating + '<i class="fa fa-star"></i>');
-        $(".thumb-next")
-            .find("span")
-            .next()
-            .html(nextRating + '<i class="fa fa-star"></i>');
-    });
-    $(".thumb-next").on("click", function () {
-        feedbackSlider.trigger("next.owl.carousel", [300]);
-        return false;
-    });
-    $(".thumb-prev").on("click", function () {
-        feedbackSlider.trigger("prev.owl.carousel", [300]);
-        return false;
-    });
-}); //end ready
-
+const myswiper = new Swiper(".swiper", {
+  speed: 800, // スライドが切り替わる時の速さ
+  slidesPerView: "3", // スライド表示数
+  spaceBetween: 50, // スライド間の距離
+  loop: true, // ループさせる
+  centeredSlides: true, // スライドを中央に配置にする
+  autoplay: {
+    // 自動再生させる
+    delay: 1500, // 次のスライドまでの秒数
+    disableOnInteraction: true // ユーザーがスライダー操作後も自動再生し続ける
+  }
+});
 
 // 
 
