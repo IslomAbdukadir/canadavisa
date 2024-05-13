9deb1872b95b077afcf0b236d3778324914d2c43
islo <islomabduqodir@gmail.com>
2024-05-13 23:40:05 +0500

    sad

--- js/script.js
+++ js/script.js
@@ -50,22 +50,7 @@ const myswiper = new Swiper(".swiper", {
     }
 });
 
-// 
-
-$(document).ready(
-    function () {
-        $(".log-in").click(
-            function () {
-                $(".panel_call").animate({
-                    width: 'toggle'
-                }, 350)
-                return false;
-            }
-        )
-    }
-);
-
-// 
+//  
 
 
 
